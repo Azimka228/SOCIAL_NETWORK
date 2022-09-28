1f5b7f3384e985d7e2cb080c7c36369f3d7cf3e3
Azimka <71658005+Azimka228@users.noreply.github.com>
2022-09-29 00:54:09 +0300

    Refactor code, install "React buddy", Map in components

--- src/Components/Content/Profile/Profile.tsx
+++ src/Components/Content/Profile/Profile.tsx
@@ -5,12 +5,34 @@ import AboutUser from "./AboutUser/AboutUser";
 import AddPost from "./AddPost/AddPost";
 
 const Profile = () => {
+
+	const posts = [
+		{
+			avatar: "https://klike.net/uploads/posts/2019-05/1556708032_1.jpg",
+			message: "Hi",
+			likes: 3,
+		},
+		{
+			avatar: "https://klike.net/uploads/posts/2019-05/1556708032_1.jpg",
+			message: "Hello",
+			likes: 12,
+		},
+		{
+			avatar: "https://klike.net/uploads/posts/2019-05/1556708032_1.jpg",
+			message: "Wow",
+			likes: 2,
+		}
+	]
+
+	let PostsElements = posts.map((post) => (
+		<div><Post avatar={post.avatar} message={post.message} likes={post.likes}/></div>
+	))
+
 	return (
 		<div className={s.Content}>
 			<AboutUser/>
 			<AddPost/>
-			<Post  avatar={'https://klike.net/uploads/posts/2019-05/1556708032_1.jpg'} message={'Hi'} likes={3}/>
-			<Post  avatar={'https://klike.net/uploads/posts/2019-05/1556708032_1.jpg'} message={'LOX'} likes={1}/>
+			{PostsElements}
 		</div>
 	);
 };
